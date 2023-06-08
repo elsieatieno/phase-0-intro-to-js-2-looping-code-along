16d30a5c28525943cd0d0d1c428afc9bf155f8c1
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-06-08 11:04:56 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solutions in this file
+/*
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
+}
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);*/
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+const namesCards =[...names, "surprise"];
+
+
+
+function writeCards(names){
+  const newArray = []
+  for (let i = 0; i < names.length; i++){
+    let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+    newArray.push(message)
+    
+  }
+  return newArray;
+}
+console.log(writeCards(names));
+
+function countDown(){
+  let countDown = 0;
+  while(countDown < 11){
+    console.log(countDown ++);
+  }
+}
